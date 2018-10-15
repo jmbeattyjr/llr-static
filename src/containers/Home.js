d2686dde5b9a8081e47700e966e5967a587e77e9
mbeatty <jmbjr@1791.com>
2018-10-15 12:16:09 -0400

    do it

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,7 @@ import logoImg from '../logo.png'
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to TEST</h1>
+    <h1 style={{ textAlign: 'center' }}>Welcome to TEST AHHH</h1>
     <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
   </div>
 ))
